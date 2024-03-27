2f76eb53914cb22af5d9d481c84f8a6265e13c31
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-03-27 15:28:33 -0300

    Update 1.3-Change-Internet.java

--- 1-Part-Java-Language/1-Chapter-History/1.3-Change-Internet.java
+++ 1-Part-Java-Language/1-Chapter-History/1.3-Change-Internet.java
@@ -1,6 +1,15 @@
 /*
 *   How Java Changed the Internet
 **      Java Applets
+            is a special kind of Java program that is designed to be transmitted over the Internet and automatically executed by a java-compatible web browser.
+            Furthermore, an applet is downloaded on demand, ithout further interaction with the user.
+                If the user clicks an link that contains an applet, the applet will be automatically downloaded and run in the browser.
+            The creation of the applet changed Internet programming because it expanded th universe of objects that can move about freely in cyberspace.
+                Passiv | c° Dynimic
+                Dynimic, networked programs are, they also present serious problems in the areas of security and portability.
+                Java solved these problems in a effective and elegant way.
 **      Security
 **      Portability
+** 43/1313
+** file:///L:/GitHub/Java/_Book/javabook.pdf
 */
